885f5c75e997dbfeb43117d1a9e3bc2089a75f20
Tygran <tygran@example.com>
2023-08-06 22:04:52 +0300

    UC#42 Start

---
+++ src/main/java/org/example/driver/DriverManager.java
@@ -0,0 +1,24 @@
+package org.example.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver getWebDriver() {
+        if (driver.get() == null) {
+            WebDriverManager.chromedriver().setup();
+            driver.set(new ChromeDriver());
+        }
+
+        return driver.get();
+    }
+
+    public static void quitWebDriver() {
+        driver.get().quit();
+        driver.set(null);
+    }
+}
